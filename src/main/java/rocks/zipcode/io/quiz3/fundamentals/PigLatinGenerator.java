563eb7949402fe98e6e6fc535ab90f2187ecdc1a
Edward Korkollie <korkollieed@gmail.com>
2019-06-25 08:41:46 -0400

    update

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -1,10 +1,31 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+
+        char[]  stringArray = str.toCharArray();
+        int start =0;
+        int firstVowel = 0;
+        int end =str.length();
+
+        for(int i =0; i< end; i++){
+
+            char c= Character.toLowerCase(str.charAt(i));
+            if(Arrays.asList(stringArray).contains(c)){
+                firstVowel =i;
+                break;
+            }
+        }
+        if(start != firstVowel) {
+            String startString = str.substring(firstVowel, end);
+            String endString = str.substring(start, firstVowel) + "ay";
+            return startString + endString;
+        }
+        return str;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -1,6 +1,8 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
@@ -8,18 +10,42 @@ import java.util.List;
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+
+       //String[] array = new String[] {str};
+        String[] array =str.split("");
+        //System.out.println(Arrays.asList(array));
+        String[] output = new String[str.length()];
+        for (int i = 0; i< array.length; i++){
+            output[i] =array[i];
+            if(i == indexToCapitalize){
+
+                output[i] = array[i].toUpperCase();
+            }
+
+        }
+
+        return String.join("", output);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+
+
+        return baseString.charAt(indexOfString) == characterToCheckFor;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        LinkedHashSet<String> substring = new LinkedHashSet<>();
+        for(int i = 0; i<string.length(); i++){
+            for(int j = i+1; j <=string.length(); j++){
+                substring.add(string.substring(i, j));
+            }
+        }
+
+        return substring.toArray(new String[1]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+
+        return getAllSubStrings(input).length;
     }
 }
